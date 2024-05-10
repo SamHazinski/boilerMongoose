05176129b77df6097ac443eafaec02f79993c06b
Sam Hazinski <shazinski@gmail.com>
2024-05-10 14:22:05 -0400

    fixed connection

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
-const mongoose = require('mongoose');
+const { connect, connection } = require('mongoose');
 
+const connectionString = 'mongodb://127.0.0.1:27017/studentsDB';
 
-mongoose.connect('mongodb://127.0.0.1/boiler')
+connect(connectionString);
 
-
-module.exports = mongoose.connection; 
+module.exports = connection;
